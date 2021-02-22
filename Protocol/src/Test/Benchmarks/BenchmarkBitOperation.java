41347c7d6b23fd05cfcbedf9c550ec218ec183c0
mvargas33 <mvargas33@protonmail.com>
2021-02-22 02:56:14 -0300

    Added new benchmarks results

--- Protocol/src/Test/Benchmarks/BenchmarkBitOperation.java
+++ Protocol/src/Test/Benchmarks/BenchmarkBitOperation.java
@@ -8,7 +8,8 @@ import java.util.concurrent.TimeUnit;
 
 public class BenchmarkBitOperation {
 
-    // Results: Intel i59300H avgt 20.696 +- 0.951 ns/op
+    // Results: Intel i59300H       avgt 20.696 +- 0.951 ns/op
+    // Results: Intel i59300H(2)    avgt 10.284 +- 0.249 ns/op
     // ponerValorEnArray se llama muchas veces en updateByteArrayFromValues
     // int -> byte[]
     @Benchmark
@@ -23,7 +24,8 @@ public class BenchmarkBitOperation {
         BitOperations.ponerValorEnArray(raw, desdeBit, val, bitSig);
     }
 
-    // Results: Intel i59300H avgt 54.624 +- 2.081 ns/op
+    // Results: Intel i59300H       avgt 54.624 +- 2.081 ns/op
+    // Results: Intel i59300H(2)    avgt 52.488 +- 1.873 ns/op
     // int[] -> byte[]
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
@@ -40,7 +42,8 @@ public class BenchmarkBitOperation {
         BitOperations.updateByteArrayFromValues(source, rawBytes, bitSig, bitSigInicio, rawBytes_inicio, rawBytes_fin);
     }
 
-    // Results: Intel i59300H avgt 10.656 +- 0.184 ns/op
+    // Results: Intel i59300H       avgt 10.656 +- 0.184 ns/op
+    // Results: Intel i59300H(2)    avgt 10.742 +- 0.317 ns/op
     // extraerBits se llama muchas veces en updateValuesFromByteArray
     // byte[] -> int
     @Benchmark
@@ -51,7 +54,8 @@ public class BenchmarkBitOperation {
         int res = BitOperations.extraerBits(raw, 10, 8); // desde segundo indice en byte[]
     }
 
-    // Results: Intel i59300H avgt 29.884 +- 0.456 ns/op
+    // Results: Intel i59300H       avgt 29.884 +- 0.456 ns/op
+    // Results: Intel i59300H(2)    avgt 29.489 +- 0.074 ns/op
     // byte[] -> int[]
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
@@ -67,7 +71,8 @@ public class BenchmarkBitOperation {
         BitOperations.updateValuesFromByteArray(destino, rawBytes, bitSig, bitSigInicio, rawBytes_inicio, rawBytes_fin);
     }
 
-    // Results: Intel i59300H avgt 7.022 +- 0.149 ns/op
+    // Results: Intel i59300H       avgt 7.022 +- 0.149 ns/op
+    // Results: Intel i59300H(2)    avgt 6.432 +- 0.044 ns/op
     // Se usa harto antes de enviar cada mensaje, para hacer clean de bytes en mensaje antes de insertar valores
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
