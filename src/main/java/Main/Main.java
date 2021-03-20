91dcb547c6d86eddcf0b847affc0cab75ffd8337
mvargas33 <mvargas33@protonmail.com>
2021-03-20 14:57:52 -0300

    Solved dependencies. Migrated to standard directory structure

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,116 +1,107 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package Main;
 
 import ApplicationLayer.AppComponents.AppComponent;
 import ApplicationLayer.AppComponents.AppReceiver;
 import ApplicationLayer.AppComponents.AppSender;
+import ApplicationLayer.AppComponents.ExcelToAppComponent.CSVToAppComponent;
 import ApplicationLayer.LocalServices.PrintService;
 import ApplicationLayer.LocalServices.WirelessService.WirelessReceiver;
 import ApplicationLayer.LocalServices.WirelessService.WirelessSender;
 import ApplicationLayer.LocalServices.WirelessService.ZigBeeLayer.Receiving.XbeeReceiver;
-import ApplicationLayer.SensorReading.RandomReaders.RandomReader;
 import ApplicationLayer.SensorReading.SensorsReader;
 import ApplicationLayer.SensorReading.SequentialReaderExecutor;
-import ApplicationLayer.AppComponents.ExcelToAppComponent.CSVToAppComponent;
-
+import ApplicationLayer.SensorReading.RandomReaders.RandomReader;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
-
-    //public String dir = "Eolian-Auriga-backend/Protocol/src/ApplicationLayer.AppComponents.ExcelToAppComponent/Eolian_fenix";
-    public String dir = "../src/main/java/ApplicationLayer/AppComponents/ExcelToAppComponent/Eolian_fenix";
-
+    public String dir = "/Github/Eolian-Auriga-backend/src/main/java/ApplicationLayer/AppComponents/ExcelToAppComponent/Eolian_fenix";
     public XbeeReceiver xbeeReceiver;
 
-    public static AppSender findAppSender(List<AppSender> list, String componentID) throws Exception{
-        for (AppSender a: list
-             ) {
-            if (a.getID().equals(componentID))
-                return a;
-        }
-        throw new Exception("Component with ID " + componentID + " was not found in AppSender list.");
+    public Main() {
     }
 
-    /**
-     * Se lanza esta funcion al iniciar la RPI.
-     * @param args
-     */
+    public static AppSender findAppSender(List<AppSender> list, String componentID) throws Exception {
+        Iterator var2 = list.iterator();
+
+        AppSender a;
+        do {
+            if (!var2.hasNext()) {
+                throw new Exception("Component with ID " + componentID + " was not found in AppSender list.");
+            }
+
+            a = (AppSender)var2.next();
+        } while(!a.getID().equals(componentID));
+
+        return a;
+    }
+
     public static void main(String[] args) throws Exception {
-        // todo: poner opcion de terminar programa
         Main main_program = new Main();
         main_program.receiverSetup();
         main_program.senderSetup();
     }
 
     void receiverSetup() throws Exception {
-        // ojo con este path!!
-        //String dir = "../ApplicationLayer.AppComponents.ExcelToAppComponent/Eolian_fenix";
-
-        List<AppReceiver> appReceivers = CSVToAppComponent.CSVs_to_AppReceivers(dir);
-
-        // High Level Services
+        List<AppReceiver> appReceivers = CSVToAppComponent.CSVs_to_AppReceivers(this.dir);
         PrintService printService = new PrintService();
         WirelessReceiver wirelessReceiver = new WirelessReceiver(appReceivers);
+        this.xbeeReceiver = wirelessReceiver.getXbeeReceiver();
+        Iterator var4 = appReceivers.iterator();
 
-        this.xbeeReceiver = wirelessReceiver.getXbeeReceiver(); // Save globally to pass to xbeeSender
+        while(var4.hasNext()) {
+            AppComponent ac = (AppComponent)var4.next();
+            ac.subscribeToService(printService);
+        }
 
-        for (AppComponent ac: appReceivers) {ac.subscribeToService(printService); }
-
-        // Execute threads
         ExecutorService mainExecutor = Executors.newFixedThreadPool(2);
-
-        // Init threads
-        //mainExecutor.submit(wirelessReceiver); // Crea 2 threads más
         mainExecutor.submit(wirelessReceiver.getXbeeReceiver());
         mainExecutor.submit(wirelessReceiver.getReceiverAdmin());
         mainExecutor.submit(printService);
-
         mainExecutor.shutdown();
     }
 
-    /**
-     * Execute afet recevierSetup due to xbeeReceiver to pass to xbeeSender (testing only)
-     * @throws Exception
-     */
     void senderSetup() throws Exception {
-        // ojo con estos paths antes de correr el main!!
-        //String dir = "../ApplicationLayer.AppComponents.ExcelToAppComponent/Eolian_fenix";
+        List<AppSender> appSenders = CSVToAppComponent.CSVs_to_AppSenders(this.dir);
+        LinkedList<RandomReader> randomReaders = new LinkedList();
+        Iterator var3 = appSenders.iterator();
 
-
-        List<AppSender> appSenders = CSVToAppComponent.CSVs_to_AppSenders(dir);
-
-        LinkedList<RandomReader> randomReaders = new LinkedList<>();
-
-        // Random reader
-        for (AppSender as: appSenders) {
-            randomReaders.add(new RandomReader(as, 1000));
+        while(var3.hasNext()) {
+            AppSender as = (AppSender)var3.next();
+            randomReaders.add(new RandomReader(as, 1000L));
         }
 
-        // Sequential executor of readers
         SequentialReaderExecutor sequentialReaderExecutor = new SequentialReaderExecutor();
-        for (SensorsReader sr : randomReaders) { sequentialReaderExecutor.addReader(sr); }
+        Iterator var9 = randomReaders.iterator();
 
-        // High Level Services
+        while(var9.hasNext()) {
+            SensorsReader sr = (SensorsReader)var9.next();
+            sequentialReaderExecutor.addReader(sr);
+        }
+
         PrintService printService = new PrintService();
-        WirelessSender wirelessSender = new WirelessSender(appSenders, xbeeReceiver);
+        WirelessSender wirelessSender = new WirelessSender(appSenders, this.xbeeReceiver);
+        Iterator var6 = appSenders.iterator();
 
-        for (AppComponent ac: appSenders) {
+        while(var6.hasNext()) {
+            AppComponent ac = (AppComponent)var6.next();
             ac.subscribeToService(printService);
             ac.subscribeToService(wirelessSender);
         }
 
-        // Execute threads
         ExecutorService mainExecutor = Executors.newFixedThreadPool(4);
-
-        // Init threads
-        mainExecutor.submit((wirelessSender.getXbeeSender()));  // XbeeSender thred
-        mainExecutor.submit(wirelessSender);                    // serve(AppComponent) to putInXbeeQueue() thread
-
+        mainExecutor.submit(wirelessSender.getXbeeSender());
+        mainExecutor.submit(wirelessSender);
         mainExecutor.submit(sequentialReaderExecutor);
         mainExecutor.submit(printService);
-
         mainExecutor.shutdown();
     }
 }
