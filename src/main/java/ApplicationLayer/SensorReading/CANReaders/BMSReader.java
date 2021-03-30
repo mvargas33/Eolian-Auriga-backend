4b6f4fb62d1a20f6ef4c63fdcac2b3f05e69733a
mvargas33 <mvargas33@protonmail.com>
2021-03-30 17:39:31 -0300

    Avance con BMS data

--- src/main/java/ApplicationLayer/SensorReading/CANReaders/BMSReader.java
+++ src/main/java/ApplicationLayer/SensorReading/CANReaders/BMSReader.java
@@ -13,19 +13,19 @@ import java.io.InputStreamReader;
  */
 public class BMSReader extends SensorsReader {
     private byte[] data = new byte[8];
-    private int[] voltages = new int[256];
-    private int[] temp_all_the_time = new int[256];
-    private int[] temp_when_balacing_off = new int[256];
-    private int[] resistance = new int[256];
+    private double[] voltages = new double[256];
+    private double[] temp_all_the_time = new double[256];
+    private double[] temp_when_balacing_off = new double[256];
+    private double[] resistance = new double[256];
 
-    private int[] voltage_reading_ok = new int[256];
-    private int[] temperature_reading_ok = new int[256];
-    private int[] resistance_reading_ok = new int[256];
-    private int[] load_is_on = new int[256];
-    private int[] voltage_sensor_fault = new int[256];
-    private int[] temperature_sensor_fault = new int[256];
-    private int[] resistance_calculation_fault = new int[256];
-    private int[] load_fault = new int[256];
+    private double[] voltage_reading_ok = new double[256];
+    private double[] temperature_reading_ok = new double[256];
+    private double[] resistance_reading_ok = new double[256];
+    private double[] load_is_on = new double[256];
+    private double[] voltage_sensor_fault = new double[256];
+    private double[] temperature_sensor_fault = new double[256];
+    private double[] resistance_calculation_fault = new double[256];
+    private double[] load_fault = new double[256];
 
     public BMSReader(AppSender myComponent, long readingDelayInMS) {
         super(myComponent, readingDelayInMS);
@@ -47,81 +47,82 @@ public class BMSReader extends SensorsReader {
         switch (msg[2]){
             case "622":
                 // State of system
-                int fault_state  = data[0] & 0b00000001;
-                int K1_contactor = data[0] & 0b00000010;
-                int K2_contactor = data[0] & 0b00000100;
-                int K3_contactor = data[0] & 0b00001000;
+                double fault_state  = (int) data[0] & 0b00000001;
+                double K1_contactor = ((int) data[0] & 0b00000010) >> 1;
+                double K2_contactor = ((int) data[0] & 0b00000100) >> 2;
+                double K3_contactor = ((int) data[0] & 0b00001000) >> 3;
+                double relay_fault = ((int) data[0] & 0b00010000) >> 4;
 
-                int powerup_time = ((data[1] << 8) & data[2]);
+                double powerup_time = (((int) data[1] << 8) & (int) data[2]); // [s]
 
                 // Byte of flags
-                int power_from_source = data[3] & 0b00000001;
-                int power_from_load = data[3] & 0b00000010;
-                int interlock_tripped = data[3] & 0b00000100;
-                int hard_wire_contactor_request = data[3] & 0b00001000;
-                int can_contactor_request = data[3] & 0b00010000;
-                int HLIM_set = data[3] & 0b00100000;
-                int LLIM_set = data[3] & 0b01000000;
-                int fan_on = data[3] & 0b10000000;
+                double power_from_source = (int) data[3] & 0b00000001;
+                double power_from_load = ((int) data[3] & 0b00000010) >> 1;
+                double interlock_tripped = ((int) data[3] & 0b00000100) >> 2;
+                double hard_wire_contactor_request = ((int) data[3] & 0b00001000) >> 3;
+                double can_contactor_request = ((int) data[3] & 0b00010000) >> 4;
+                double HLIM_set = ((int) data[3] & 0b00100000) >> 5;
+                double LLIM_set = ((int) data[3] & 0b01000000) >> 6;
+                double fan_on = ((int) data[3] & 0b10000000) >> 7;
 
                 // Fault code, stored
-                int fault_code = data[4];
+                double fault_code = (int) data[4];
 
                 // Level fault flags
-                int driving_off_while_plugged_in = data[5] & 0b00000001;
-                int interlock_tripped2 = data[5] & 0b00000010;
-                int comm_fault_blank_or_cell = data[5] & 0b00000100;
-                int charge_overcurrent = data[5] & 0b00001000;
-                int discharge_overcurrent = data[5] & 0b00010000;
-                int over_temp = data[5] & 0b00100000;
-                int under_voltage = data[5] & 0b01000000;
-                int over_voltage = data[5] & 0b10000000;
+                double driving_off_while_plugged_in = (int) data[5] & 0b00000001;
+                double interlock_tripped2 = ((int) data[5] & 0b00000010) >> 1;
+                double comm_fault_blank_or_cell = ((int) data[5] & 0b00000100) >> 2;
+                double charge_overcurrent = ((int) data[5] & 0b00001000) >> 3;
+                double discharge_overcurrent = ((int) data[5] & 0b00010000) >> 4;
+                double over_temp = ((int) data[5] & 0b00100000) >> 5;
+                double under_voltage = ((int) data[5] & 0b01000000) >> 6;
+                double over_voltage = ((int) data[5] & 0b10000000) >> 7;
 
                 // Warnings flags
-                int low_voltage = data[6] & 0b00000001;
-                int high_voltage = data[6] & 0b00000010;
-                int charge_overcurrent_warning = data[6] & 0b00000100;
-                int discharge_overcurrent_warning = data[6] & 0b00001000;
-                int cold_temp = data[6] & 0b00010000;
-                int hot_temp = data[6] & 0b00100000;
-                int low_SOH = data[6] & 0b01000000;
-                int isolateion_fault = data[6] & 0b10000000;
+                double low_voltage = (int) data[6] & 0b00000001;
+                double high_voltage = ((int) data[6] & 0b00000010) >> 1;
+                double charge_overcurrent_warning = ((int) data[6] & 0b00000100) >> 2;
+                double discharge_overcurrent_warning = ((int) data[6] & 0b00001000) >> 3;
+                double cold_temp = ((int) data[6] & 0b00010000) >> 4;
+                double hot_temp = ((int) data[6] & 0b00100000) >> 5;
+                double low_SOH = ((int) data[6] & 0b01000000) >> 6;
+                double isolateion_fault = ((int) data[6] & 0b10000000) >> 7;
 
             case "623":
-                int pack_voltage = ((data[0] << 8) & data[1]); // [kV]
-                int min_voltage = data[2]; // [100mV]
-                int min_voltage_id = data[3];
-                int max_voltage = data[4];
-                int max_voltage_id = data[5];
+                double pack_voltage = (((int) data[0] << 8) & (int) data[1]); // [V] [0,65535]
+                double min_voltage = (int) data[2]/10.0; // [100mV] [0, 255] -> [V] [0.0,25.5]
+                double min_voltage_id = (int) data[3]; // [0,255]
+                double max_voltage = (int) data[4]/10.0; // [100mV] [0, 255] -> [V] [0.0,25.5]
+                double max_voltage_id = (int) data[5]; // [0,255]
 
             case "624":
-                int current = ((data[0] << 8) & data[1]); // [A] signed!
-                int charge_limit = ((data[2] << 8) & data[3]); // [A]
-                int discharge_limit = ((data[4] << 8) & data[5]); // [A]
+                double current = (((int) data[0] << 8) & (int) data[1]); // [A] signed! [-32764, 32764]
+                double charge_limit = (((int) data[2] << 8) & (int) data[3]); // [A] [0, 65535]
+                double discharge_limit = (((int) data[4] << 8) & (int) data[5]); // [A]
 
             case "625":
-                int batt_energy_in  = (data[0] << 8*3) & (data[1] << 8*2) & (data[2] << 8) & data[3]; // [kWh]
-                int batt_energy_out = (data[4] << 8*3) & (data[5] << 8*2) & (data[6] << 8) & data[7];
+                double batt_energy_in  = ((int) data[0] << 8*3) & ((int) data[1] << 8*2) & ((int) data[2] << 8) & (int) data[3]; // [kWh][0,4294967295]
+                double batt_energy_out = ((int) data[4] << 8*3) & ((int) data[5] << 8*2) & ((int) data[6] << 8) & (int) data[7]; // [kWh][0,4294967295]
 
             case "626":
-                int SOC = data[0];
-                int DOD = ((data[1] << 8) & data[2]);
-                int capacity = ((data[3] << 8) & data[4]);
-                int SOH = data[6];
+                double SOC = (int) data[0]; // [%] [0,100]
+                double DOD = (((int) data[1] << 8) & (int) data[2]); // [AH] [0,65535]
+                double capacity = (((int) data[3] << 8) & (int) data[4]); // [AH] [0,65535]
+                double SOH = (int) data[6]; // [%] [0,100]
 
             case "627":
-                int temperature = data[0];
-                int min_temp = data[2];
-                int min_temp_id = data[3];
-                int max_temp = data[4];
-                int max_temp_id = data[5];
+                double temperature = (int) data[0]; // [C] signed! [-127,127]
+                double min_temp = (int) data[2]; // [C] signed! [-127,127]
+                double min_temp_id = (int) data[3];
+                double max_temp = (int) data[4]; // [C] signed! [-127,127]
+                double max_temp_id = (int) data[5];
 
             case "628":
-                int pack_resistance = ((data[0] << 8) & data[1]);
-                int min_resistance = data[2];
-                int min_resistance_id = data[3];
-                int max_resistance = data[4];
-                int max_resistance_id = data[5];
+                double pack_resistance = (((int) data[0] << 8) & (int) data[1])/10.0; // [100 micro-ohm][0,65525] -> [milli ohm] [0.0,6552.5]
+                double min_resistance = ((int) data[2])/10.0; // [100 micro-ohm][0,255] -> [milli ohm] [0.0,25.5]
+                double min_resistance_id = (int) data[3];
+                double max_resistance = (int) data[4]; // [100 micro-ohm][0,255] -> [milli ohm] [0.0,25.5]
+                double max_resistance_id = (int) data[5];
 
             default:
                 int BASE_DUMP_ID = 0;
@@ -129,35 +130,35 @@ public class BMSReader extends SensorsReader {
 
                 // Individual cell details
                 if (id == BASE_DUMP_ID){
-                    this.voltages[data[0]] = data[1]; // mV
-                    this.temp_all_the_time[data[0]] = data[2]; // °C
-                    this.temp_when_balacing_off[data[0]] = data[3]; // °C
-                    this.resistance[data[0]] = data[4]; // 100 uOhm
+                    this.voltages[data[0]] = (((int) data[1]) + 2.0)/100.0 ; // [10mV] [200, 455] -> [V] [2.00, 4.55]
+                    this.temp_all_the_time[data[0]] = ((int) data[2]) - 128; // °C [-128, 127]
+                    this.temp_when_balacing_off[data[0]] = ((int) data[3]) - 128; // °C [-128, 127]
+                    this.resistance[data[0]] = ((int) data[4])/10.0; // [100 microOhm] [0,255] -> [miliOhm] [0.0, 25.5]
                     // Status
-                    this.voltage_reading_ok[data[0]] = data[5] & 0b00000001;
-                    this.temperature_reading_ok[data[0]] = data[5] & 0b00000010;
-                    this.resistance_reading_ok[data[0]] = data[5] & 0b00000100;
-                    this.load_is_on[data[0]] = data[5] & 0b00001000;
-                    this.voltage_sensor_fault[data[0]] = data[5] & 0b00010000;
-                    this.temperature_sensor_fault[data[0]] = data[5] & 0b00100000;
-                    this.resistance_calculation_fault[data[0]] = data[5] & 0b01000000;
-                    this.load_fault[data[0]] = data[5] & 0b10000000;
+                    this.voltage_reading_ok[data[0]] = (int) data[5] & 0b00000001;
+                    this.temperature_reading_ok[data[0]] = ((int) data[5] & 0b00000010) >> 1;
+                    this.resistance_reading_ok[data[0]] = ((int) data[5] & 0b00000100) >> 2;
+                    this.load_is_on[data[0]] = ((int) data[5] & 0b00001000) >> 3;
+                    this.voltage_sensor_fault[data[0]] = ((int) data[5] & 0b00010000) >> 4;
+                    this.temperature_sensor_fault[data[0]] = ((int) data[5] & 0b00100000) >> 5;
+                    this.resistance_calculation_fault[data[0]] = ((int) data[5] & 0b01000000) >> 6;
+                    this.load_fault[data[0]] = ((int) data[5] & 0b10000000) >> 7;
                 }
 
                 // Cell voltages
                 else if (id > BASE_DUMP_ID && id <= (BASE_DUMP_ID+32)){
-                    this.voltages[((id-BASE_DUMP_ID-1)*8)] = data[0];
-                    this.voltages[((id-BASE_DUMP_ID-1)*8)+1] = data[1];
-                    this.voltages[((id-BASE_DUMP_ID-1)*8)+2] = data[2];
-                    this.voltages[((id-BASE_DUMP_ID-1)*8)+3] = data[3];
-                    this.voltages[((id-BASE_DUMP_ID-1)*8)+4] = data[4];
-                    this.voltages[((id-BASE_DUMP_ID-1)*8)+5] = data[5];
-                    this.voltages[((id-BASE_DUMP_ID-1)*8)+6] = data[6];
-                    this.voltages[((id-BASE_DUMP_ID-1)*8)+7] = data[7];
+                    this.voltages[((id-BASE_DUMP_ID-1)*8)  ] = (((int) data[0]) + 2.0)/100.0; // [10mV] [200, 455] -> [V] [2.00, 4.55]
+                    this.voltages[((id-BASE_DUMP_ID-1)*8)+1] = (((int) data[1]) + 2.0)/100.0;
+                    this.voltages[((id-BASE_DUMP_ID-1)*8)+2] = (((int) data[2]) + 2.0)/100.0;
+                    this.voltages[((id-BASE_DUMP_ID-1)*8)+3] = (((int) data[3]) + 2.0)/100.0;
+                    this.voltages[((id-BASE_DUMP_ID-1)*8)+4] = (((int) data[4]) + 2.0)/100.0;
+                    this.voltages[((id-BASE_DUMP_ID-1)*8)+5] = (((int) data[5]) + 2.0)/100.0;
+                    this.voltages[((id-BASE_DUMP_ID-1)*8)+6] = (((int) data[6]) + 2.0)/100.0;
+                    this.voltages[((id-BASE_DUMP_ID-1)*8)+7] = (((int) data[7]) + 2.0)/100.0;
                 }
 
         }
-        System.out.print("ID: " + msg[2] + " MSG: ");
+        System.out.println("ID: " + msg[2] + " MSG: ");
         for(int i=8 ; i< 16; i++){
             System.out.print(" " + msg[i]);
 //        switch (msg[0])
